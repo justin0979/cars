8f87dd32790ecf052d0b44c07cda2ac414d53e71
justin0979 <justinm0979@gmail.com>
2023-06-21 15:25:02 -0500

    add bold text to adding cars

--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -1,13 +1,23 @@
 import { useDispatch, useSelector } from "react-redux";
-import { CarsState, CarType, removeCar } from "../store";
+import { FormType, CarsState, CarType, removeCar } from "../store";
 
 function CarList() {
   const dispatch = useDispatch();
-  const cars = useSelector(
-    ({ cars: { data, searchTerm } }: { cars: CarsState }) => {
-      return data.filter((car) =>
+  const { cars, name } = useSelector(
+    ({
+      form,
+      cars: { data, searchTerm },
+    }: {
+      form: FormType;
+      cars: CarsState;
+    }) => {
+      const filteredCars = data.filter((car) =>
         car.name.toLowerCase().includes(searchTerm.toLowerCase()),
       );
+      return {
+        cars: filteredCars,
+        name: form.name,
+      };
     },
   );
 
@@ -18,8 +28,12 @@ function CarList() {
   };
 
   const renderedCars = cars.map((car) => {
+    // DECIDE IF THIS CAR SHOULD BE BOLD
+    const bold =
+      name && car.name.toLowerCase().includes(name.toLowerCase());
+
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${bold && "bold"}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
