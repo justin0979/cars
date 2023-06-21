e1760d1b5022d7a5f763dd012c6898d8178055dd
justin0979 <justinm0979@gmail.com>
2023-06-21 14:35:55 -0500

    add changing state of search term

--- src/components/CarSearch.tsx
+++ src/components/CarSearch.tsx
@@ -1,7 +1,32 @@
+import { useDispatch, useSelector } from "react-redux";
+import { CarsState, changeSearchTerm } from "../store";
+
 function CarSearch() {
+  const dispatch = useDispatch();
+  const searchTerm = useSelector(
+    ({ cars }: { cars: CarsState }) => cars.searchTerm,
+  );
+
+  const handleSearchTermChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    dispatch(changeSearchTerm(event.target.value));
+  };
+
   return (
-    <div>
-      <h1>Car Search</h1>
+    <div className="list-header">
+      <h3 className="title is-3">My Cars</h3>
+      <div className="search field is-horizontal">
+        <label className="label" htmlFor="">
+          Search
+        </label>
+        <input
+          className="input"
+          type="string"
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+        />
+      </div>
     </div>
   );
 }
