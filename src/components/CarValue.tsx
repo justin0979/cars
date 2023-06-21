dfaf813d5d5182b966af989967a493337fafdd2c
justin0979 <justinm0979@gmail.com>
2023-06-21 15:15:26 -0500

    add cost functionality

--- src/components/CarValue.tsx
+++ src/components/CarValue.tsx
@@ -1,9 +1,18 @@
+import { useSelector } from "react-redux";
+import { CarsState } from "../store";
+
 function CarValue() {
-  return (
-    <div>
-      <h1>Car Value</h1>
-    </div>
+  const totalCost = useSelector(
+    ({ cars: { data, searchTerm } }: { cars: CarsState }) => {
+      return data
+        .filter((car) =>
+          car.name.toLowerCase().includes(searchTerm.toLowerCase()),
+        )
+        .reduce((total, { cost }) => total + cost, 0);
+    },
   );
+
+  return <div className="car-value">Total Cost: ${totalCost}</div>;
 }
 
 export default CarValue;
