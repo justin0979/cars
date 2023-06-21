56a3b6b6d9935e83d182cd79508bc137a1f374b5
justin0979 <justinm0979@gmail.com>
2023-06-21 12:05:17 -0500

    show cars list and remove individual car

--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -1,7 +1,38 @@
+import { useDispatch, useSelector } from "react-redux";
+import { CarsState, CarType, removeCar } from "../store";
+
 function CarList() {
+  const dispatch = useDispatch();
+  const cars = useSelector(({ cars }: { cars: CarsState }) => {
+    return cars.data;
+  });
+
+  const handleCarDelete = (car: CarType) => {
+    if (car.id) {
+      dispatch(removeCar(car.id));
+    }
+  };
+
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+
   return (
-    <div>
-      <h1>Car List</h1>
+    <div className="car-list">
+      {renderedCars}
+      <hr />
     </div>
   );
 }
